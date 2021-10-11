cf9ef10b0a1633960569760859a4d316912efab3
jazzmbarry <jazzmbarry@gmail.com>
2021-10-11 16:52:19 -0600

    seeds file working

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,25 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department_table (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE role_table (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department_table(id)
 );
 
 CREATE TABLE employee_table (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES role_table(id)
+    FOREIGN KEY (role_id) REFERENCES role_table(id),
+    FOREIGN KEY (manager_id) REFERENCES employee_table(id)
 );
 
